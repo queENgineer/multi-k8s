9f5bdf9ac9c787fd5453559901cf59a135343f76
queENgineer <queENgineer>
2022-05-30 17:27:40 +0300

    docker id changed

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -33,7 +33,7 @@ class Fib extends Component{
     }
 
     renderSeenIndexes(){
-        return this.state.seenIndexes.map(({number}) => number).join('.');
+        return this.state.seenIndexes.map(({number}) => number).join(',');
     }
     
     renderValues(){
